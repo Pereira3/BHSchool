1d23e3317d2fcaefc35135fd1087f4ae86cd7838
Pereira3 <ricardojapereira@gmail.com>
2024-05-16 14:42:15 +0100

    Initial commit

---
+++ src/test/java/com/example/pratico/PraticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pratico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
